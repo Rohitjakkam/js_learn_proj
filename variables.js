0b8d18eff28a585d8e87863e1062cdc7a8de3847
Rohit Jakkam <86354086+Rohitjakkam@users.noreply.github.com>
2023-06-14 15:08:45 +0000

    update

--- variables.js
+++ variables.js
@@ -2,9 +2,9 @@ const accountId =12212
 let accountEmail="rohitjakkam@gmail.com"
 var accountPassword="1112"
 accountCity = "Thane"
-
+let status=null
 // accountEmail= "jsjsjsjksk"
 // accountId =2
 // accountPassword="2232"
 // console.log(accountEmail);
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,status]);
